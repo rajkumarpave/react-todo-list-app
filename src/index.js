6f85756f3e6e56b2ddda34f265c63fd82722047e
Raj Kumar D <rajkumar.d@instellars.com>
2024-04-01 15:49:18 +0530

    ADD: Bootstrap CSS File

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 // // add bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
